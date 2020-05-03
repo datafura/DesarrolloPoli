29997860349019a23dae8d9c860cd6086ebbc5cd
daniel mateo tafur astroz <danielmateotafurastroz@MacBook-Pro.local>
2020-05-02 22:45:42 -0500

    creacion de plantilla node js para aplicativo semilleros

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+//configuracion de servidor 
+app.set('port', 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs'); 
+
+//middlewares
+
+
+//importacion de rutas
+app.use(require('./routes/'));
+
+
+
+
+//archivos estaticos
+app.use(express.static(path.join(__dirname, 'public')))
+ 
+
+//escuchando el servidor en el puerto 4000 
+app.listen(app.get('port'), () => {
+console.log('servidor funcionando en el puerto:', app.get('port'));
+});
